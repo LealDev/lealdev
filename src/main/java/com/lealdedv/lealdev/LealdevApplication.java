73b88c14b867505734d4979633e3e451054a3358
LealDev <lealmaster0111@gmail.com>
2024-01-04 19:46:25 -0300

    primeiro commit do projeto

---
+++ src/main/java/com/lealdedv/lealdev/LealdevApplication.java
@@ -0,0 +1,13 @@
+package com.lealdedv.lealdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LealdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LealdevApplication.class, args);
+	}
+
+}
