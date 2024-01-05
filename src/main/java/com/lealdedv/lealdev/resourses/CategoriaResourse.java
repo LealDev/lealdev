a886090d8ad72e2c239ebda0575e133040936e4f
LealDev <lealmaster0111@gmail.com>
2024-01-05 16:05:45 -0300

    testando a primeira classe: Categoria

--- src/main/java/com/lealdedv/lealdev/resourses/CategoriaResourse.java
+++ src/main/java/com/lealdedv/lealdev/resourses/CategoriaResourse.java
@@ -1,15 +1,28 @@
 package com.lealdedv.lealdev.resourses;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lealdedv.lealdev.domain.Categoria;
 
 
 @RestController
 public class CategoriaResourse {
     
     @GetMapping(value = "/categorias")
-    public String listar() {
-        return "REST está funcionando";
+    public List<Categoria> listar() {
+        Categoria cat1 = new Categoria(1,"categoria 1");
+        Categoria cat2 = new Categoria(2,"categoria 2");
+
+        List list =  new ArrayList<>();
+
+        list.add(cat1);
+        list.add(cat2);
+        
+        return list;
     }    
 
 }
